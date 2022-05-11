3c5831620ad7a6c6cc4a4821b3ad9407bac8d115
yastrebow <yastrebow@gmail.com>
2022-05-11 16:13:37 +0300

    Add conveyor

---
+++ conveyor/src/main/java/ru/yastrebov/loanconveyor/conveyor/ConveyorApplication.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.loanconveyor.conveyor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveyorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConveyorApplication.class, args);
+    }
+
+}
---
+++ conveyor/src/test/java/ru/yastrebov/loanconveyor/conveyor/ConveyorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.loanconveyor.conveyor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
